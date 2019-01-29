68d1db4f2be0435378030d7e83293f77480dc252
defnngj <353332934@qq.com>
2019-01-29 18:38:59 +0800

    update

--- test_dir/test_baidu.py
+++ test_dir/test_baidu.py
@@ -13,7 +13,7 @@ class TestSearch:
         """ 百度搜索：pytest """
         page = BaiduPage(browser)
         page.get(base_url)
-        page.search_input.send_keys("pytest")
+        page.search_input = "pytest"
         page.search_button.click()
         sleep(2)
         assert browser.title == "pytest_百度搜索"
@@ -30,26 +30,26 @@ class TestSearch:
         """百度搜索 --参数化"""
         page = BaiduPage(browser)
         page.get(base_url)
-        page.search_input.send_keys(search_key)
+        page.search_input = search_key
         page.search_button.click()
         sleep(2)
         assert browser.title == search_key+"_百度搜索"
 
 
-# class TestSearchSettings:
-#     """百度搜索设置"""
-#
-#     def test_baidu_search_setting(self, browser, base_url):
-#         """百度搜索设置"""
-#         page = BaiduPage(browser)
-#         page.get(base_url)
-#         page.settings.click()
-#         page.search_setting.click()
-#         sleep(2)
-#         page.save_setting.click()
-#         alert_text = browser.switch_to.alert.text
-#         browser.switch_to.alert.accept()
-#         assert alert_text == "已经记录下您的使用偏好"
+class TestSearchSettings:
+    """百度搜索设置"""
+
+    def test_baidu_search_setting(self, browser, base_url):
+        """百度搜索设置"""
+        page = BaiduPage(browser)
+        page.get(base_url)
+        page.settings.click()
+        page.search_setting.click()
+        sleep(2)
+        page.save_setting.click()
+        alert_text = page.get_alert_text
+        browser.switch_to.alert.accept()
+        assert alert_text == "已经记录下您的使用偏好"
 
 
 if __name__ == '__main__':
